a80e79c1bf066cf721fd6038510f5031cd1539e1
tanayabichandani <tanay2@illinois.edu>
2020-04-29 20:03:53 -0500

    Ball no longer bounces on ground, but no friction on ground

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -9,7 +9,7 @@
 
 namespace myapp {
 
-const float kGravitationalConstant = -9.81;
+const float kGravitationalConstant = -9.81f;
 const float kPixelToMeters = 0.1f;
 const float kBallRadius = 2.0f;
 const int kWindowWidth = 1000;
@@ -35,14 +35,29 @@ void MyApp::setup() {
 }
 
 void MyApp::update() {
-  float time_step = 1.0f / 60.0f;
+  float time_step = 1.0 / 30.0f;
   int velocity_iterations = 6;
   int position_iterations = 2;
   world_->Step(time_step, velocity_iterations, position_iterations);
 
   auto position = ball_body_->GetPosition();
-  if (position.y < 0 && !is_velocity_changed_) {
-    auto velocity_vector = ball_body_->GetLinearVelocity();
+  auto box_width = (float) kWindowWidth * kPixelToMeters - kBallRadius;
+  auto box_height = (float) (kWindowHeight - kCeilingSize - kGroundSize)
+                    * kPixelToMeters - kBallRadius;
+
+  auto velocity_vector = ball_body_->GetLinearVelocity();
+  if (position.y > box_height && !is_velocity_changed_) {
+    velocity_vector.y = -0.6f * velocity_vector.y;
+    ball_body_->SetLinearVelocity(velocity_vector);
+    is_velocity_changed_ = true;
+  } else if ((position.x < kBallRadius || position.x > box_width)
+             && !is_velocity_changed_) {
+    velocity_vector.x = -0.6f * velocity_vector.x;
+    ball_body_->SetLinearVelocity(velocity_vector);
+    is_velocity_changed_ = true;
+  } else if (position.y < 2.0f && !is_velocity_changed_) {
+    ball_body_->SetTransform({position.x, 2.0},
+                             ball_body_->GetAngle());
     velocity_vector.y = -0.6f * velocity_vector.y;
     ball_body_->SetLinearVelocity(velocity_vector);
     is_velocity_changed_ = true;
@@ -69,50 +84,59 @@ void MyApp::draw() {
                                                y_pos + radius));
 }
 
-void MyApp::keyDown(KeyEvent event) { }
+void MyApp::keyDown(KeyEvent event) {
+  switch (event.getCode()) {
+    case KeyEvent::KEY_UP: {
+      ball_body_->SetLinearVelocity({0, 30});
+      break;
+    }
+    case KeyEvent::KEY_RIGHT: {
+      ball_body_->SetLinearVelocity({30, 0});
+      break;
+    }
+    case KeyEvent::KEY_LEFT: {
+      ball_body_->SetLinearVelocity({-30, 0});
+      break;
+    }
+  }
+}
 
 void MyApp::InitWorld() {
-  // Generating world
+  // Choosing screen size
+  setWindowSize(kWindowWidth, kWindowHeight);
+
+  // Generating box2d world
   b2Vec2 gravity(0.0f, kGravitationalConstant);
   world_ = std::make_unique<b2World>(gravity);
 
   // Making the 4 walls by putting static body rectangle around screen window
-  b2BodyDef screen_border_def;
-  screen_border_def.position.Set(0.0f, 0.0f);
-  b2Body* screen_border = world_->CreateBody(&screen_border_def);
-  screen_border->SetType(b2_staticBody);
-  b2EdgeShape screen_border_shape;
-
-  setWindowSize(kWindowWidth, kWindowHeight);
-  // Code for generating screen boundaries, does not properly work yet
+  // Code currently does not work
   /**auto box_width = (float) kWindowWidth * kPixelToMeters;
   auto box_height = (float) (kWindowHeight - kCeilingSize - kGroundSize)
                     * kPixelToMeters;
-  b2Vec2 lower_left_corner(0.0f, 0.0f);
-  b2Vec2 lower_right_corner(box_width, 0.0f);
-  b2Vec2 upper_left_corner(0.0f, box_height);
-  b2Vec2 upper_right_corner(box_width, box_height);
 
-  screen_border_shape.Set(lower_left_corner, lower_right_corner);
-  screen_border->CreateFixture(&screen_border_shape, 0.0f);
+  b2BodyDef screen_border_def;
+  screen_border_def.type = b2_staticBody;
+  screen_border_def.position.Set(box_width / 2.0f, 4.4955f);
+  b2Body* screen_border = world_->CreateBody(&screen_border_def);
 
-  screen_border_shape.Set(lower_right_corner, upper_right_corner);
-  screen_border->CreateFixture(&screen_border_shape, 0.0f);
+  b2PolygonShape border_shape;
+  border_shape.SetAsBox(box_width / 2.0f, box_height / 2.0f);
 
-  screen_border_shape.Set(upper_right_corner, upper_left_corner);
-  screen_border->CreateFixture(&screen_border_shape, 0.0f);
-
-  screen_border_shape.Set(upper_left_corner, lower_left_corner);
-  screen_border->CreateFixture(&screen_border_shape, 0.0f);*/
-
+  b2FixtureDef fixture_def;
+  fixture_def.shape = &border_shape;
+  fixture_def.density = 1;
+  screen_border->CreateFixture(&fixture_def);
+  // Just creating ground
   b2BodyDef ground_body_def;
-  ground_body_def.position.Set((float) kWindowWidth / 2 * kPixelToMeters,
-                               35.496f);
+  ground_body_def.type = b2_staticBody;
+  ground_body_def.position.Set(0.0f,
+                               29.4954f);
+  ground_body_def.angle = 0.0f;
   b2Body* ground_body = world_->CreateBody(&ground_body_def);
   b2PolygonShape ground_box;
-  ground_box.SetAsBox((float) kWindowWidth / 2, 5.0f);
-  //ground_body->CreateFixture(&ground_box, 0.0f);
-
+  ground_box.SetAsBox(10000.0f, 5.0f);
+  ground_body->CreateFixture(&ground_box, 0.0f);*/
 
   // Creating the soccer ball
   b2BodyDef ball_body_def;
@@ -128,6 +152,7 @@ void MyApp::InitWorld() {
 
   b2FixtureDef ball_fixture_def;
   ball_fixture_def.shape = &circle;
+  ball_fixture_def.restitution = 0.6f;
   ball_fixture_def.density = 1.0f;
   ball_fixture_def.friction = 0.3f;
 
@@ -155,7 +180,7 @@ void MyApp::DrawBackground() {
 
   // Drawing goals in
   cinder::gl::color(1, 1, 1);
-  auto goal_height = goal_image_->getActualHeight();
+  auto goal_height = (int) (1.3 * goal_image_->getActualHeight());
   auto goal_width = goal_image_->getActualWidth();
   // Right goal
   cinder::gl::draw(goal_image_, cinder::Rectf(
